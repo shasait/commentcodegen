6295e2c787ffa3480177f596a033d06c1708242b
concentus <hasait@web.de>
2005-10-06 19:28:50 +0000

    - redesign of add/remove nature code

---
+++ de.hasait.ccg/src/de/hasait/ccg/action/AddCcgNatureAction.java
@@ -0,0 +1,84 @@
+/*
+ * $Id: AddCcgNatureAction.java,v 1.1 2005-10-06 19:28:49 a-pi Exp $
+ * 
+ * Copyright 2005 Sebastian Hasait
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.hasait.ccg.action;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+import de.hasait.ccg.builder.CcgNature;
+import de.hasait.ccg.util.NatureUtil;
+
+/**
+ * @author Sebastian Hasait (hasait at web.de)
+ * @version $Revision: 1.1 $
+ */
+public class AddCcgNatureAction implements IObjectActionDelegate {
+    private ISelection _selection;
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
+     */
+    public void run(IAction action) {
+        if (_selection instanceof IStructuredSelection) {
+            Object element = ((IStructuredSelection) _selection)
+                    .getFirstElement();
+            IProject project = null;
+            if (element instanceof IProject) {
+                project = (IProject) element;
+            } else if (element instanceof IAdaptable) {
+                project = (IProject) ((IAdaptable) element)
+                        .getAdapter(IProject.class);
+            }
+            if (project != null) {
+                try {
+                    NatureUtil.addNature(project, CcgNature.NATURE_ID);
+                } catch (CoreException e) {
+                    // ignore
+                }
+            }
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action.IAction,
+     *      org.eclipse.jface.viewers.ISelection)
+     */
+    public void selectionChanged(IAction action, ISelection selection) {
+        _selection = selection;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.IObjectActionDelegate#setActivePart(org.eclipse.jface.action.IAction,
+     *      org.eclipse.ui.IWorkbenchPart)
+     */
+    public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+        // ignore
+    }
+}
