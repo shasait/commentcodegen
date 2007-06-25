c63ea4f8d308a230995e6fe37cca7724253565bb
concentus <hasait@web.de>
2007-06-25 16:59:46 +0000

    - Test

--- ccgtest/src/Test123.java
+++ ccgtest/src/Test123.java
@@ -2,94 +2,52 @@
 
 public class Test123 {
 	// [c]
-	// <property type="String" name="foo" cardinality="*" hooked="true" />
+	// <property type="String" name="foo5" final="true" />
 	// [/c]
 	// [bs]f3ee87c7c0a8001901c041ff851f0f40[/bs]
 
 	/**
-	 *  Property foo
+	 *  Property foo5
 	 */
-	public static final String PROPERTYNAME_FOO = "foos";
-	/** The many property field foo. */
-	private final java.util.List _foos = new java.util.ArrayList();
-
+	public static final String PROPERTYNAME_FOO5 = "foo5";
+	/** The single property field foo5. */
+	private String _foo5;
+	
 	/**
-	 * Returns the array of all foos.
+	 * Returns the foo5.
 	 */
-	public final String[] getFoos() {
-			return (String[])_foos.toArray(new String[_foos.size()]);
+	public final String getFoo5() {
+			return _foo5;
 	}
-
+	
 	/**
-	 * Returns the foo at the specified index.
+	 * Sets the foo5.
 	 */
-	public final String getFoo(final int index) {
-			return (String)_foos.get(index);
-	}
-
-	/**
-	 * Returns if the specified foo is contained.
-	 */
-	public final boolean containsFoo(final String foo) {
-			return _foos.contains(foo);
-	}
-
-	/**
-	 * Returns how many foos are contained.
-	 */
-	public final int fooSize() {
-			return _foos.size();
-	}
-
-	/**
-	 * Returns if foo is empty.
-	 */
-	public final boolean isFooEmpty() {
-			return _foos.isEmpty();
-	}
-
-	/**
-	 * Returns an {@link Iterator} for all foos.
-	 */
-	public final java.util.Iterator fooIterator() {
-			return _foos.iterator();
-	}
-
-	/**
-	 * Adds the specified foo.
-	 */
-	public final void addFoo(final String foo) {
-			if (_foos.contains(foo)) {
-				return;
-			}
-			_foos.add(foo);
-			newFoo(foo);
-	}
-		
-	/**
-	 * Removes the specified foo.
-	 */
-	public final void removeFoo(final String foo) {
-			if (!_foos.contains(foo)) {
-				return;
-			}
-			_foos.remove(foo);
-			oldFoo(foo);
-	}
-
-	/**
-	 * Remove all foos.
-	 */
-	public final void removeAllFoos() {
-		String[] foos;
-			foos = getFoos();
-		for (int i=0; i<foos.length; i++) {
-			removeFoo(foos[i]);
-		}
+	protected final void setFoo5(final String foo5) {
+			_foo5 = foo5;
 	}
 	// [be]f3ee87c7c0a8001901c041ff851f0f40[/be]
 
 	public final void add() {
 
 	}
+
+	// [c]
+	// <springin name="halloFoo" type="String" required="true" />
+	// [/c][bs]528dd8100a0a0b2401892bd896fbabfa[/bs]
+
+	private String _halloFoo;
+	
+	public final void setHalloFoo(final String halloFoo) {
+		_halloFoo = halloFoo;
+	}
+	
+	protected final String useHalloFoo() {
+		if (_halloFoo == null) {
+			throw new IllegalArgumentException("halloFoo == null");
+		}
+		return _halloFoo;
+	}
+
+	// [be]528dd8100a0a0b2401892bd896fbabfa[/be]	
 }
